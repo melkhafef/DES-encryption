e35cef69cf890fb70efdf6dec02afb49b26371be
melkhafef <42751023+melkhafef@users.noreply.github.com>
2018-11-20 13:57:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ const expantionDbox = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12,
 const sbox = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7, 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8, 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0, 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13, 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10, 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5, 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15, 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9, 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8, 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1, 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7, 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12, 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15, 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9, 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4, 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14, 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9, 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6, 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14, 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3, 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11, 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8, 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6, 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13, 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1, 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6, 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2, 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12, 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7, 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2, 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8, 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
 const straightDbox = [15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24];
 const finalPermutation = [39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48, 16, 56, 24];
-const symbolsToBinary = [
-  {
+const symbolsToBinary = [{
     "symbol": "\u0000",
     "binary": "00000000"
   },
@@ -73,11 +72,11 @@ const symbolsToBinary = [
     "binary": "00001011"
   },
   {
-    "symbol": "\f",
+    "symbol": "Ā",
     "binary": "00001100"
   },
   {
-    "symbol": "\r",
+    "symbol": "ā",
     "binary": "00001101"
   },
   {
@@ -134,7 +133,7 @@ const symbolsToBinary = [
   },
   {
     "symbol": "\u001b",
-    "binary": "00011010"
+    "binary": "00011011"
   },
   {
     "symbol": "\u001c",
@@ -1049,6 +1048,7 @@ const symbolsToBinary = [
     "binary": "11111111"
   }
 ]
+  
 
 caesar.addEventListener('click', (e) => {
     playFairKey.disabled = true;
@@ -1303,6 +1303,7 @@ encrypteBtn.addEventListener('click', (e) => {
             final = [];
             rightFun = [];
             finalBinary = [];
+            finalB = '';
             encryptedMessage.value='';
             if (plainMessage.value.length%8 !==0){
                 for (d = 0; d < 8 - (plainMessage.value.length % 8);d++){
@@ -1345,7 +1346,6 @@ encrypteBtn.addEventListener('click', (e) => {
                 keyCompression.push(key);
             }
             for(m=0;m<plainBlocks.length;m++){
-                console.log(`plain before : ${plainBlocks[m]}`)
             plainBlocks[m] = permute(plainBlocks[m].split(''), initialPermutation);
             plainBlocks[m] = plainBlocks[m].join('');
             left = plainBlocks[m].substring(0, 32);
@@ -1360,12 +1360,14 @@ encrypteBtn.addEventListener('click', (e) => {
             }
             final = right + left;
             final =permute(final.split(''),finalPermutation);
-            final=final.join('');
+              final = final.join('');
+            finalB+=final;
+          }
             finalBinary = [];
-            for (e = 0; e < 8; e++) {
-                finalBinary.push(final.substring(e * 8, (e + 1) * 8));
+          for (e = 0; e < finalB.length/8; e++) {
+                finalBinary.push(finalB.substring(e * 8, (e + 1) * 8));
             }
-                console.log(`encrypt after : ${finalBinary.join('')}`)
+                // console.log(`encrypt after : ${finalBinary.join('')}`)
             encryptedtext+=(convertToText(finalBinary));
             left='';
             right='';
@@ -1374,7 +1376,7 @@ encrypteBtn.addEventListener('click', (e) => {
             final=[];
             rightFun=[];
             finalBinary = [];
-        }
+        
             encryptedMessage.value = encryptedtext;
             plainMessage.value='';
         }
@@ -1660,7 +1662,6 @@ decrypteBtn.addEventListener('click', (e) => {
             }
 
             for (m = 0; m < encryptBlocks.length; m++) {
-                console.log(`encrypte before : ${encryptBlocks[m]}`)
             encryptBlocks[m] = permute(encryptBlocks[m].split(''), initialPermutation);
             encryptBlocks[m] = encryptBlocks[m].join('');
             left = encryptBlocks[m].substring(0, 32);
@@ -1681,7 +1682,6 @@ decrypteBtn.addEventListener('click', (e) => {
             for (e = 0; e < 8; e++) {
                 finalBinary.push(final.substring(e * 8, (e + 1) * 8));
             }
-                console.log(`encrypt after : ${finalBinary.join('')}`)
             plainText += convertToText(finalBinary);
                 left = '';
                 right = '';
@@ -1790,11 +1790,11 @@ function xOR(num1, num2, length) {
 
 function convertToText(string) {
     stringText = '';
+   
     for (p = 0; p < string.length; p++) {
         for (a = 0; a < symbolsToBinary.length; a++) {
             if (string[p] === symbolsToBinary[a].binary) {
-                stringText += symbolsToBinary[a].symbol;
-break;
+                stringText+=symbolsToBinary[a].symbol;
             }
         }
     }
